7c2f572bfffc54419db72d8677b42b01393fe1c2
unknown <euodiatqy@gmail.com>
2026-02-02 17:19:39 +0800

    add all projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,17 +4,66 @@ import ProjectCard from "./UI/ProjectCard";
 const PROJECTS = [
   {
     name: "GovTech Singapore – Web Emissions Prototype",
-    description:
-      "During my time in GovTech, I developed a full-stack web prototype to analyse carbon emissions and identify optimisation opportunities (media, code, files) for reducing the carbon footprint of government websites.",
+    description: [
+      "I researched and developed a full-stack web prototype to analyse carbon emissions and identify optimisation opportunities for reducing the carbon footprint of government websites. ",
+      "This was enhanced and adopted by the GovTech team, and later presented as a solution at the {build} Hackathon 2025, with endorsement from the Minister of State (MOH & MDDI).",
+    ],
     stack: "React · Express · DynamoDB · AWS",
     images: ["/src/assets/images/govtechgif.gif"],
+    link: "https://build.tech.gov.sg/projects/eco-mode",
   },
   {
     name: "Kunyah Cafe - Mobile Ordering Interface",
-    description: 
-        "For my university's Final Year Project, I designed and developed a self-ordering mobile interface for an F&B establishment employing people with disabilities.",
+    description: [
+      "For my university's Final Year Project, I designed and developed a self-ordering mobile interface for an F&B establishment employing people with physical disabilities, minimising manual customer interaction and allowing staff to focus on food preparation.",
+    ],
     stack: "React · Node.js · PostgreSQL · AWS",
     images: ["/src/assets/images/kunyahmobilegif.gif"],
+    link: "",
+  },
+  {
+    name: "Kunyah Cafe - Corporate Website Prototype",
+    description:["For my university's Final Year Project, I prototyped a corporate website for a F&B establishment that hires employees with disabilities."],
+    stack: "Figma · Canva",
+    images: ["/src/assets/images/kunyahwebsite.png"],
+    link: "",
+  },
+  {
+    name: "Internet-of-Things: Patient Wearable Heart Rate Monitor Dashboard",
+    description:["For a Cloud Management & Engineering module, I developed a mock doctor's dashboard to monitor the status of patients wearing heart rate monitoring devices."],
+    stack: "HTML · CSS · Chart.JS · AWS S3, Sagemaker, Glue",
+    images: ["/src/assets/images/heatmap2.jpg"],
+    link: "",
+  },
+   {
+    name: "Asteroid Website Tracker",
+    description:["I designed and developed a website that calls NASA's open database to retrieve asteroid information. It allows users to input a data range and get a list of potentially hazardous asteroids that are flying near Earth."],
+    stack: "CSS · EJS",
+    images: ["/src/assets/images/asteroid-tracker.png"],
+    trylink: "https://asteroid-tracker-yixg.onrender.com/",
+    mobile: false
+  },
+  {
+    name: "Book Notes Website",
+    description:["I designed and developed a website that allows you to record important notes and information about books that you have read. It automatically retrieves book covers with valid ISBN."],
+    stack: "CSS · EJS · PostgreSQL ",
+    images: ["/src/assets/images/book-notes-bigger.png", "/src/assets/images/book-notes3.png"],
+    trylink: "https://booknotes-kx6w.onrender.com/",
+    mobile: false
+  },
+  {
+    name: "Blog Website",
+    description:["I designed and developed a website that allows you to create, edit and delete blog posts."],
+    stack: "HTML · CSS · EJS",
+    images: ["/src/assets/images/blog-bigger.png"],
+    trylink: "https://blog-website-2gbl.onrender.com/",
+    mobile: false
+  },
+  {
+    name: "Peer to Peer Bank Transaction Mobile App",
+    description:["I designed a P2P bank transaction mobile application for users that want an easy and reliable way to send money, that will inform them of the process at every step of the user journey."],
+    stack: "Figma · Canva",
+    images: ["/src/assets/images/bank8.png"],
   },
 
 ];
@@ -24,12 +73,16 @@ export default function Projects() {
     <section className={classes.container} id="projects">
       <h2>My Projects</h2>
       <div className={classes.projectContainer}>
-        {PROJECTS.map((project)=> (
+        {PROJECTS.map((project) => (
           <ProjectCard
+            key={project.name}
             name={project.name}
             description={project.description}
             stack={project.stack}
             images={project.images}
+            link={project.link}
+            trylink={project.trylink}
+            mobileResponsive={project.mobile}
           />
         ))}
       </div>
--- src/components/UI/ProjectCard.jsx
+++ src/components/UI/ProjectCard.jsx
@@ -1,16 +1,19 @@
-import classes from './ProjectCard.module.css';
-import UIUXCalculatorThumbnail from '../../assets/images/govtechgif.gif';
+import classes from "./ProjectCard.module.css";
+import UIUXCalculatorThumbnail from "../../assets/images/govtechgif.gif";
 
-export default function ProjectCard({name, description, stack, images}){
-    return (
-      
-        <div className={classes.card}>
-            {images.map((image)=> (
-                <img src={image}/>
-            ))}
-            <h2>{name}</h2>
-            <h3>{description}</h3>
-            <p>{stack}</p>
-        </div>
-    );
+export default function ProjectCard({ name, description, stack, images, link, trylink, mobileResponsive }) {
+  return (
+    <div className={classes.card}>
+      {images.map((image) => (
+        <img key={image} src={image} />
+      ))}
+      <h2>{name}</h2>
+      {description.map((paragraph) => (
+        <h3 key={paragraph}>{paragraph}</h3>
+      ))}
+      {link && <a href={link}>Read more →</a>}
+      {trylink && <a href={trylink}>View live demo {mobileResponsive ? "[ Mobile/Desktop ]" : "[ Desktop ]"} →</a>}
+      <p>{stack}</p>
+    </div>
+  );
 }
